989251fd52a1475235ecfe662d5f0d647c7ac46f
Daasoham <119830645+Tribhuvanachar@users.noreply.github.com>
2026-07-27 17:15:38 +0530

    Update granthaBookmarks.js

--- dge/js/granthaBookmarks.js
+++ dge/js/granthaBookmarks.js
@@ -1,1 +1,96 @@
+/*
+=========================================================
+Digital Grantha Engine
+Grantha Bookmarks
+Version: 10.1.0 Alpha
+=========================================================
+*/
 
+class GranthaBookmarks {
+
+    constructor() {
+
+        this.storageKey = "dge_bookmarks";
+
+        this.bookmarks = [];
+
+    }
+
+    initialize() {
+
+        this.load();
+
+    }
+
+    load() {
+
+        const saved = localStorage.getItem(this.storageKey);
+
+        if (saved) {
+
+            this.bookmarks = JSON.parse(saved);
+
+        }
+
+    }
+
+    save() {
+
+        localStorage.setItem(
+
+            this.storageKey,
+
+            JSON.stringify(this.bookmarks)
+
+        );
+
+    }
+
+    add(verseId) {
+
+        if (!this.bookmarks.includes(verseId)) {
+
+            this.bookmarks.push(verseId);
+
+            this.save();
+
+        }
+
+    }
+
+    remove(verseId) {
+
+        this.bookmarks = this.bookmarks.filter(
+
+            id => id !== verseId
+
+        );
+
+        this.save();
+
+    }
+
+    has(verseId) {
+
+        return this.bookmarks.includes(verseId);
+
+    }
+
+    getAll() {
+
+        return [...this.bookmarks];
+
+    }
+
+    clear() {
+
+        this.bookmarks = [];
+
+        this.save();
+
+    }
+
+}
+
+window.GranthaBookmarks =
+    new GranthaBookmarks();
