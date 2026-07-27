f6d13cb832bb190cbe31cddde0150a94d47ba874
Daasoham <119830645+Tribhuvanachar@users.noreply.github.com>
2026-07-27 18:57:42 +0530

    Update granthaManager.js

--- dge/js/granthaManager.js
+++ dge/js/granthaManager.js
@@ -1,88 +1,17 @@
-/*
-=========================================================
-Digital Grantha Engine
-Grantha Manager
-=========================================================
-*/
-
 class DGEGranthaManager {
 
-    constructor() {
-        this.version = {};
-        this.catalog = {};
-        this.current = {};
-        this.config = {};
-        this.dataset = [];
-    }
-
     async start() {
-
-        await this.loadVersion();
-        await this.loadCatalog();
-        await this.loadCurrent();
-        await this.loadConfig();
-        await this.loadDataset();
-
-    }
-
-    async loadVersion() {
-
-        const r = await fetch("version.json");
-        this.version = await r.json();
-
-    }
-
-    async loadCatalog() {
-
-        const r = await fetch("data/granthas.json");
-        this.catalog = await r.json();
-
-    }
-
-    async loadCurrent() {
-
-        this.current =
-            this.catalog.granthas.find(
-
-                g => g.id === this.catalog.defaultGrantha
-
-            );
-
-    }
-
-    async loadConfig() {
-
-        const r =
-            await fetch(this.current.config);
-
-        this.config =
-            await r.json();
-
-    }
-
-    async loadDataset() {
-
-        const r =
-            await fetch(this.current.dataset);
-
-        this.dataset =
-            await r.json();
-
-    }
-
-    getTitle() {
-
-        return this.config.title;
-
+        console.log("DGE GranthaManager started");
     }
 
     getDataset() {
+        return [];
+    }
 
-        return this.dataset;
-
+    getTitle() {
+        return "Digital Grantha Engine";
     }
 
 }
 
-window.DGEGranthaManager =
-    new DGEGranthaManager();
+window.DGEGranthaManager = new DGEGranthaManager();
