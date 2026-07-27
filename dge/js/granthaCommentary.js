3351a290102d4939aaa1a409544406fefa53fdf4
Daasoham <119830645+Tribhuvanachar@users.noreply.github.com>
2026-07-27 21:39:53 +0530

    Update granthaCommentary.js

--- dge/js/granthaCommentary.js
+++ dge/js/granthaCommentary.js
@@ -2,32 +2,93 @@
 =========================================================
 Digital Grantha Engine
 Grantha Commentary
-Build 018
+Build 019
 =========================================================
 */
 
 document.body.insertAdjacentHTML(
     "afterbegin",
-    '<div style="background:#7b1fa2;color:white;padding:6px;font-family:monospace">granthaCommentary.js BUILD 018</div>'
+    '<div style="background:#7B1FA2;color:white;padding:6px;font-family:monospace">granthaCommentary.js BUILD 019</div>'
 );
 
 class DGEGranthaCommentary {
 
-    getEnabled(index, dataset) {
+    constructor() {
 
-        if (!dataset || !dataset[index]) {
+        this.dataset = [];
+        this.enabled = {};
 
+    }
+
+    initialize(dataset) {
+
+        console.log("COMMENTARY INITIALIZE CALLED");
+
+        this.dataset = dataset || [];
+
+    }
+
+    getNames(index) {
+
+        if (!this.dataset[index])
             return [];
 
-        }
+        return Object.keys(
+            this.dataset[index].commentary || {}
+        );
 
-        const commentary = dataset[index].commentary || {};
+    }
 
-        return Object.entries(commentary).map(([name, text]) => ({
+    get(index,name){
+
+        return (
+            this.dataset[index]?.commentary?.[name]
+            || ""
+        );
+
+    }
+
+    enable(name){
+
+        this.enabled[name]=true;
+
+    }
+
+    disable(name){
+
+        delete this.enabled[name];
+
+    }
+
+    isEnabled(name){
+
+        return !!this.enabled[name];
+
+    }
+
+    enableAll(index){
+
+        this.getNames(index).forEach(name=>{
+
+            this.enabled[name]=true;
+
+        });
+
+    }
+
+    disableAll(){
+
+        this.enabled={};
+
+    }
+
+    getEnabled(index){
+
+        return this.getNames(index).map(name=>({
 
             name,
 
-            text
+            text:this.get(index,name)
 
         }));
 
@@ -35,4 +96,10 @@ class DGEGranthaCommentary {
 
 }
 
-window.DGEGranthaCommentary = new DGEGranthaCommentary();
+window.DGEGranthaCommentary =
+    new DGEGranthaCommentary();
+
+console.log(
+    "COMMENTARY OBJECT =",
+    window.DGEGranthaCommentary
+);
