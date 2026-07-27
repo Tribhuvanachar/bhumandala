c0aed83942fd03c6c41fd14b834d43309d4a9fd6
Daasoham <119830645+Tribhuvanachar@users.noreply.github.com>
2026-07-27 22:21:15 +0530

    Update granthaAudio.js

--- dge/js/granthaAudio.js
+++ dge/js/granthaAudio.js
@@ -2,140 +2,56 @@
 =========================================================
 Digital Grantha Engine
 Grantha Audio
-Version: 10.1.0 Alpha
+Build 022
 =========================================================
 */
 
-class GranthaAudio {
+document.body.insertAdjacentHTML(
+    "afterbegin",
+    '<div style="background:#3F51B5;color:#fff;padding:6px;font-family:monospace">granthaAudio.js BUILD 022</div>'
+);
 
-    constructor() {
+class DGEGranthaAudio {
 
-        this.player = null;
+    constructor(){
 
-        this.dataset = [];
-
-        this.currentIndex = 0;
-
-    }
-
-    initialize(playerId = "audioPlayer") {
-
-        this.player =
-            document.getElementById(playerId);
+        this.player=null;
 
     }
 
-    load(dataset) {
+    initialize(){
 
-        this.dataset = dataset || [];
+        this.player=document.getElementById("audioPlayer");
 
     }
 
-    play(index = this.currentIndex) {
+    play(url){
 
-        if (!this.player)
-            return;
+        if(!this.player) return;
 
-        if (index < 0)
-            return;
-
-        if (index >= this.dataset.length)
-            return;
-
-        this.currentIndex = index;
-
-        const verse =
-            this.dataset[index];
-
-        if (!verse.audio)
-            return;
-
-        this.player.src =
-            verse.audio;
+        this.player.src=url;
 
         this.player.play();
 
     }
 
-    pause() {
+    stop(){
 
-        if (!this.player)
-            return;
-
-        this.player.pause();
-
-    }
-
-    stop() {
-
-        if (!this.player)
-            return;
+        if(!this.player) return;
 
         this.player.pause();
 
-        this.player.currentTime = 0;
-
     }
 
-    next() {
+    setRate(rate){
 
-        if (
+        if(!this.player) return;
 
-            this.currentIndex
-            <
-            this.dataset.length - 1
-
-        ) {
-
-            this.play(
-
-                this.currentIndex + 1
-
-            );
-
-        }
-
-    }
-
-    previous() {
-
-        if (
-
-            this.currentIndex > 0
-
-        ) {
-
-            this.play(
-
-                this.currentIndex - 1
-
-            );
-
-        }
-
-    }
-
-    setSpeed(speed) {
-
-        if (!this.player)
-            return;
-
-        this.player.playbackRate =
-            speed;
-
-    }
-
-    setVolume(volume) {
-
-        if (!this.player)
-            return;
-
-        this.player.volume =
-            volume;
+        this.player.playbackRate=rate;
 
     }
 
 }
 
-window.GranthaAudio =
-    new GranthaAudio();
+window.DGEGranthaAudio=
+new DGEGranthaAudio();
