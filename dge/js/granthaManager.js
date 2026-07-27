53e0f86da1d30eba23605e00965ef17e7b5b962b
Daasoham <119830645+Tribhuvanachar@users.noreply.github.com>
2026-07-27 21:46:21 +0530

    Update granthaManager.js

--- dge/js/granthaManager.js
+++ dge/js/granthaManager.js
@@ -2,11 +2,33 @@
 =========================================================
 Digital Grantha Engine
 Grantha Manager
-Build 019
+Build 020 TEST
 =========================================================
 */
 
 document.body.insertAdjacentHTML(
     "afterbegin",
-    '<div style="background:#1565c0;color:#fff;padding:6px;font-family:monospace">granthaManager.js BUILD 019</div>'
+    '<div style="background:#0066cc;color:#fff;padding:6px;font-family:monospace">granthaManager.js BUILD 020 TEST</div>'
 );
+
+console.log("granthaManager.js started");
+
+class DGEGranthaManager {
+
+    async start() {
+
+        console.log("Manager.start() called");
+
+    }
+
+    getDataset() {
+
+        return {};
+
+    }
+
+}
+
+window.DGEGranthaManager = new DGEGranthaManager();
+
+console.log("window.DGEGranthaManager =", window.DGEGranthaManager);
