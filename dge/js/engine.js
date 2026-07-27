fa36707fe308f1215d57cd4d0bdc42ab98a24c0b
Daasoham <119830645+Tribhuvanachar@users.noreply.github.com>
2026-07-27 10:49:51 +0530

    Update engine.js

--- dge/js/engine.js
+++ dge/js/engine.js
@@ -1,1 +1,20 @@
 
+/*
+ * Digital Grantha Engine
+ * Version: 10.1.0 Alpha 003
+ */
+
+window.DGE = {
+    version: "10.1.0-alpha.003",
+
+    init() {
+        console.log("================================");
+        console.log("Digital Grantha Engine Started");
+        console.log("Version:", this.version);
+        console.log("================================");
+    }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    DGE.init();
+});
